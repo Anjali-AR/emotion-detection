56af91ccac85d295174927ea714a80f1ddbbeae8
Anjali-AR <chintalpallyanjali@gmail.com>
2026-07-25 03:22:54 -0400

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,1 +1,19 @@
+import requests
 
+
+def emotion_detector(text):
+
+    url = "https://sn-watson-emotion.labs.coursera.org/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    response = requests.post(
+        url,
+        json={
+            "raw_document":{
+                "text":text
+            }
+        }
+    )
+
+    result=response.json()
+
+    return result
